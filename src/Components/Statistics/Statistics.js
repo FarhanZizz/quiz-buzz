7477c084db486534d21d02cc1abfd7cc25eabb26
FarhanZizz <iamfarhanaziz@gmail.com>
2022-10-12 23:07:43 +0600

    fixed statistics responsiveness

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
     const quizInfo = useLoaderData();
     const data = quizInfo.data;
 
     const renderBarChart = (
-        <BarChart width={600} height={300} data={data}>
-            <XAxis dataKey="name" stroke="#8884d8" />
-            <YAxis />
-            <Tooltip />
-            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <Bar dataKey="total" fill="#8884d8" barSize={30} />
-        </BarChart>
+        <div style={{ width: '100%', height: 300 }}>
+            <ResponsiveContainer>
+                <BarChart width={450} height={300} data={data}>
+                    <XAxis dataKey="name" stroke="#8884d8" />
+                    <YAxis />
+                    <Tooltip />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <Bar dataKey="total" fill="#8884d8" barSize={30} />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
     );
     return (
-        <div className='w-1/2 mx-auto'>
+        <div className='w-2/3 mx-auto '>
             <h1 className='text-4xl my-5 font-bold'>This is a chart indicating how many question does each quiz have</h1>
-            {renderBarChart}
+            <div>
+                {renderBarChart}
+            </div>
         </div>
     );
 };
